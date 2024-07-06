8ac94d170cf00fbe73e490f63b5dc28081c488c3
Manas Mohanty <manasmohanty07@gmail.com>
2024-07-06 18:47:37 +0530

    Added React code

---
+++ src/Main.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+import {
+    Route,
+    NavLink,
+    HashRouter,
+    Routes
+  } from "react-router-dom";
+  import Home from "./Home";
+  import Stuff from "./Stuff";
+  import Contact from "./Contact";
+  import USAMap from "./USAMap";
+
+  class Main extends Component {
+    render() {
+      return (
+        <HashRouter>
+        <div>
+            <h1>Simple SPA</h1>
+            <ul className="header">
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/stuff">Stuff</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
+                <li><NavLink to="/geofence">Geofence</NavLink></li>
+            </ul>
+            <div className="content">
+            <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/stuff" element={<Stuff/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                    <Route path="/geofence" element={<USAMap/>}/>
+                </Routes>
+            </div>
+        </div>
+        </HashRouter>
+      );
+    }
+  }  
+ 
+export default Main;
